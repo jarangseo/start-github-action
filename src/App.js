2491c457e4aff2abd477411ce7875d319106920a
jarangseo <rangs919@gmail.com>
2020-10-29 09:52:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+background-color: #61dafb;
+color: white;
+padding:10px;
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <StyledDiv>
+          <h1>START GITHUB ACTION FOR REACT + CLOUD FRONT</h1>
+          <logo/>
+        </StyledDiv>
       </header>
     </div>
   );
